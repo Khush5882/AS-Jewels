230eb4777cc6a4401dab16c2cf33d02e8bb56809
Sahilpreetsingh004 <Sahilpreetsingh@edu.sait.ca>
2024-09-22 16:12:42 -0600

    Update SignupModel.cs

--- Models/SignupModel.cs
+++ Models/SignupModel.cs
@@ -1,1 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ASJewellers.Models
+{
+    public class SignupModel
+    {
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
+
 
