f0ef129b6ab1c255df3ffef1c26b1f22dc4c3e4b
Sahilpreetsingh004 <Sahilpreetsingh@edu.sait.ca>
2024-09-22 15:58:56 -0600

    Update LoginModel.cs

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,1 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ASJewellers.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
 
