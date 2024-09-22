ce13a68b56bcebec81d3544f4e60237adac0ff4f
kulbirkaur3008 <142059272+kulbirkaur3008@users.noreply.github.com>
2024-09-22 16:09:22 -0600

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,1 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ASJewellers.Models;
+using System.Linq;
+
+public class HomeController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _context.Products.Take(10).ToList();
+        return View(products);
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
 
