6d56c3c0807cd79ee8b87fa24a973401ffcda490
kulbirkaur3008 <142059272+kulbirkaur3008@users.noreply.github.com>
2024-09-22 16:14:26 -0600

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,26 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace ASJewellers.Models
+public class Product
 {
-    public class SignupModel
-    {
-        [Required(ErrorMessage = "First name is required")]
-        public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Last name is required")]
-        public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
-        public string Email { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-        [Required(ErrorMessage = "Phone number is required")]
-        [Phone(ErrorMessage = "Invalid phone number")]
-        public string PhoneNumber { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Metal { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
 }
-
