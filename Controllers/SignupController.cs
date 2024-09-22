103dc86d456caf9934812c3eac51c7032837d4d2
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-09-22 16:01:56 -0600

    Update SignupController.cs

--- Controllers/SignupController.cs
+++ Controllers/SignupController.cs
@@ -1,1 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using ASJewellers.Models;
+
+namespace ASJewellers.Controllers
+{
+    public class SignupController : Controller
+    {
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Here you'd save the signup details to the database
+                // Save model details to the database
+                return RedirectToAction("Login", "Login");
+            }
+            return View(model);
+        }
+    }
+}
 
