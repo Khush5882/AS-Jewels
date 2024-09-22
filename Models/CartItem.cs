db9fb16b24e02c9991b1dd66069cda0af8307880
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-09-22 16:16:09 -0600

    Update CartItem.cs

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -1,1 +1,9 @@
+public class CartItem
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int UserId { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
 
