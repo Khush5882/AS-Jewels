efbed104170e7f39edd094d7e38aed71149c5330
kulbirkaur3008 <142059272+kulbirkaur3008@users.noreply.github.com>
2024-09-22 16:10:04 -0600

    Update ProductController.cs

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,1 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ASJewellers.Models;
+using System.Linq;
+
+public class ProductController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProductController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult ProductList()
+    {
+        var products = _context.Products.ToList();
+        return View(products);
+    }
+
+    public IActionResult ProductDetails(int id)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return View(product);
+    }
+}
 
