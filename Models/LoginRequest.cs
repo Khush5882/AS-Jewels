4c513928a13a00394718d188d4cd21c3bc58fa12
Sahilpreetsingh004 <Sahilpreetsingh@edu.sait.ca>
2024-09-23 11:50:14 -0600

    Update LoginRequest.cs

--- Models/LoginRequest.cs
+++ Models/LoginRequest.cs
@@ -1,15 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace ASJewellers.Models
+namespace jeweller_app.Models  
 {
-    public class LoginModel
+    public class LoginRequest  
     {
-        [Required(ErrorMessage = "Username is required")]
-        public string Username { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Username { get; set; } 
+        public string PasswordHash { get; set; }
+        public string Email { get; internal set; }
     }
 }
-
