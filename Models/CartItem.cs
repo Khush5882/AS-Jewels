575830a77db6d5aabff837aded88598a4c521eb3
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-09-23 11:45:18 -0600

    Update CartItem.cs

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -1,9 +1,22 @@
-public class CartItem
-{
-    public int Id { get; set; }
-    public int ProductId { get; set; }
-    public int UserId { get; set; }
-    public int Quantity { get; set; }
-    public decimal Price { get; set; }
-}
+using System;
+using System.Collections.Generic;
 
+namespace jeweller_app.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public List<CartProduct> CartItems { get; set; } = new List<CartProduct>();
+    }
+
+    public class CartProduct
+    {
+        public int Id { get; set; } // This might be necessary if you're tracking cart item IDs
+        public int ProductId { get; set; } // Assuming this is for the product being added to the cart
+        public string Email { get; set; } // User's email associated with the cart
+        public decimal Price { get; set; } // Price of the product at the time of adding to cart
+        public int Quantity { get; set; } // Quantity of the product in the cart
+    }
+
+}
