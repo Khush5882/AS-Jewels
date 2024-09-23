90aa074d966e226709dec2abad3bfc5c2a0e46ab
kulbirkaur3008 <142059272+kulbirkaur3008@users.noreply.github.com>
2024-09-23 12:31:50 -0600

    Update Instacart.cs

--- Models/Instacart.cs
+++ Models/Instacart.cs
@@ -1,1 +1,16 @@
+using System.Collections.Generic;
+using jeweller_app.Models;
+
+public class InstaCart
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public List<CartList> Lists { get; set; } = new List<CartList>();
+}
+
+public class CartList
+{
+    public string Name { get; set; }
+    public List<CartItem> Items { get; set; } = new List<CartItem>();
+}
 
