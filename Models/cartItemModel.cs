77f2ac6699cc43b99e2123f9d844073682c81360
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-09-23 12:35:34 -0600

    Update cartItemModel.cs

--- Models/cartItemModel.cs
+++ Models/cartItemModel.cs
@@ -1,1 +1,13 @@
+using jeweller_app.Models;
 
+public class CartItemModel
+{
+    public int Id { get; set; } // Unique identifier for the cart item
+    public int ProductId { get; set; } // Product ID
+    public string Email { get; set; } // User email
+    public decimal Price { get; set; } // Price at the time of adding to cart
+    public int Quantity { get; set; } // Quantity of the product
+
+    // Navigation property for related Product
+    public required Product Product { get; set; }
+}
