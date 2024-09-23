9d22d49db698c818c274316918bba4929506b6da
kulbirkaur3008 <142059272+kulbirkaur3008@users.noreply.github.com>
2024-09-23 12:05:22 -0600

    Update Order.cs

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,9 +1,11 @@
-public class Order
+namespace jeweller_app.Models
 {
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public decimal TotalAmount { get; set; }
-    public DateTime OrderDate { get; set; }
-    public string Status { get; set; }
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
 }
-
