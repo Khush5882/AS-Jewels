88565614f7b79caa71dea8dd3cb7c07b062c4462
kulbirkaur3008 <142059272+kulbirkaur3008@users.noreply.github.com>
2024-09-23 12:34:55 -0600

    Update WishlistItem.cs

--- Models/WishlistItem.cs
+++ Models/WishlistItem.cs
@@ -1,1 +1,14 @@
+namespace jeweller_app.Models
+{
+    public class WishlistItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }  // Product being added to wishlist
+        public int UserId { get; set; }  // User ID who added the product to wishlist
+
+        // Navigation properties
+        public Product Product { get; set; }
+        public User User { get; set; }
+    }
+}
 
