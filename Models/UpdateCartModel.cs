e17fafc5e646d421d9748ee60771c938c4402a65
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-09-23 12:39:51 -0600

    Update UpdateCartModel.cs

--- Models/UpdateCartModel.cs
+++ Models/UpdateCartModel.cs
@@ -1,1 +1,8 @@
-
+namespace jeweller_app.Models
+{
+    public class UpdateCartModel
+    {
+        public int CartItemId { get; set; } // ID of the cart item to update
+        public int Quantity { get; set; } // New quantity for the cart item
+    }
+}
