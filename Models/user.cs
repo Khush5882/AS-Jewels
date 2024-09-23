c1a9e7e21ce5d38d2711c667a8d8fa108f368d67
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-09-23 11:50:16 -0600

    Update user.cs

--- Models/user.cs
+++ Models/user.cs
@@ -1,26 +1,28 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace ASJewellers.Models
+namespace jeweller_app.Models
 {
-    public class SignupModel
+    public class User
     {
-        [Required(ErrorMessage = "First name is required")]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last name is required")]
+        [Required]
+        [StringLength(100)]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
 
-        [Required(ErrorMessage = "Phone number is required")]
-        [Phone(ErrorMessage = "Invalid phone number")]
-        public string PhoneNumber { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
     }
 }
-
